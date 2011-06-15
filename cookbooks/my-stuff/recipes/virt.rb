e7ab4f87472639e747937f83d6d12c5701ba8fd7
Jörg Herzinger <joerg.herzinger@oiml.at>
2011-06-15 10:29:18 +0200

    stuff for windows VMs

--- cookbooks/my-stuff/recipes/virt.rb
+++ cookbooks/my-stuff/recipes/virt.rb
@@ -1,5 +1,10 @@
 if node["cpu"]["0"]["flags"].include?("svm") || node["cpu"]["0"]["flags"].include?("vmx")
   include_recipe "my-stuff::virt_#{node.my_stuff.virt_method}"
+  
+  # Gonna need this for useable connection to Windows Nodes
+  %w( "rdesktop" "gnome-rdp" ).each do |p|
+    package p
+  end
 else
   puts "No hardware virtualization component found.  Maybe you have to enable it in your BIOS."
 end
