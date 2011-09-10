0588d3f89126dcff276e943c0fb2773b7de469cb
Jörg Herzinger <joerg.herzinger@oiml.at>
2011-09-10 11:11:56 +0200

    add ubuntu one purges

---
+++ cookbooks/my-stuff/recipes/purges.rb
@@ -0,0 +1,5 @@
+%w(ubuntuone-client ubuntuone-client-gnome ubuntuone-control-panel ubuntuone-control-panel-gtk banshee-extension-ubuntuonemusicstore python-ubuntuone-client python-ubuntuone-control-panel libubuntuone1.0-cil libubuntuone-1.0-1 libsyncdaemon-1.0-1).each do |p|
+  package p do
+    action :purge
+  end
+end
