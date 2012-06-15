c1bd92e2aee5b730dd51d791fcb38ff14275947b
Jörg Herzinger <joerg.herzinger@oiml.at>
2012-06-15 21:29:35 +0200

    no more update-manger annoyances

--- cookbooks/my-stuff/recipes/purges.rb
+++ cookbooks/my-stuff/recipes/purges.rb
@@ -1,4 +1,4 @@
-%w(ubuntuone-client ubuntuone-client-gnome ubuntuone-control-panel ubuntuone-control-panel-gtk banshee-extension-ubuntuonemusicstore python-ubuntuone-client python-ubuntuone-control-panel libubuntuone1.0-cil libubuntuone-1.0-1 libsyncdaemon-1.0-1 python-ubuntuone-storageprotocol).each do |p|
+%w(ubuntuone-client ubuntuone-client-gnome ubuntuone-control-panel ubuntuone-control-panel-gtk banshee-extension-ubuntuonemusicstore python-ubuntuone-client python-ubuntuone-control-panel libubuntuone1.0-cil libubuntuone-1.0-1 libsyncdaemon-1.0-1 python-ubuntuone-storageprotocol update-manager).each do |p|
   package p do
     action :purge
   end
